c9f00f2564874236002e1fa968ecd0a8e1467606
yTakkar <www.shtakkar@gmail.com>
2017-09-14 00:54:31 +0530

    Added linting files

--- public/js/src/main.js
+++ public/js/src/main.js
@@ -11,26 +11,26 @@ $('.register').on('submit', e => {
     password_again = $('.r_password_again').val()
 
   if(!username || !email || !password || !password_again){
-    Notify({ value: "Some values are missing!" })
+    Notify({ value: 'Some values are missing!' })
   } else {
     $.ajax({
-        url: "/user/register",
-        data: {
-          username,
-          email,
-          password,
-          password_again
-        },
-        method: "POST",
-        dataType: "JSON",
-        success: data => {
-          let { mssg, success } = data
-          console.log(data)
-          Notify({
-            value: mssg,
-            done: () => success ? location.href = "/login" : null
-          })
-        }
+      url: '/user/register',
+      data: {
+        username,
+        email,
+        password,
+        password_again
+      },
+      method: 'POST',
+      dataType: 'JSON',
+      success: data => {
+        let { mssg, success } = data
+        console.log(data)
+        Notify({
+          value: mssg,
+          done: () => success ? location.href = '/login' : null
+        })
+      }
     })
   }
 })
@@ -42,16 +42,16 @@ $('.login').on('submit', e => {
     password = $('.l_password').val()
 
   if(!username || !password){
-    Notify({ value: "Some values are missing!" })
+    Notify({ value: 'Some values are missing!' })
   } else {
     $.ajax({
-      url: "/user/login",
+      url: '/user/login',
       data: {
         username,
         password
       },
-      method: "POST",
-      dataType: "JSON",
+      method: 'POST',
+      dataType: 'JSON',
       success: data => {
         let { mssg, success, user } = data
         console.log(data)
